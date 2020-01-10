6c6f93f69e7bff32d84c708c8a10f69355a3037e
carl ind <mrnitrodown@gmail.com>
2020-01-10 09:14:45 +0000

    Successfully Collected information from the database

--- src/AccessDatabase.java
+++ src/AccessDatabase.java
@@ -1,21 +1,34 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class AccessDatabase {
     public static void main(String[] args) {
         AccessDatabase access = new AccessDatabase();
         access.createConnection();
+        access.collectData();
     }
-
+    Connection con;
 
     void createConnection(){
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/gamerandomizer", "root","my@password21");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/gamerandomizer", "root","my@password21");
             System.out.println("Connected");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void collectData(){
+        try {
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM game");
+            while (rs.next()){
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                System.out.println(id + " " + name);
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
